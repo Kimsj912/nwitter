cdad54b9c94e7e7933df59fed2ef445873825c02
sujk912 <aa991122aa@gmail.com>
2022-02-09 15:04:16 +0900

    auth.js 코드 보기좋게 정리

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,38 +2,34 @@ import React, { useState } from "react";
 import {authService, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "fBase";
 
 const Auth = () => {
-
+    // Variables with hooks
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
     const [error, setError] = useState("");
-    
-
+    // Method
     const onChange = (event) =>{
-        const {target: {name,value}} = event;
-        if(name ==="email"){
-            setEmail(value)
-        } else if(name==="password"){
-            setPassword(value)
-        }
+        // event로부터 target의 name과 value를 가져옴.
+        const {target: {name,value}} = event; 
+        //name에 따라 value를 setEmail이나 setPassword에 넣어줌
+        if(name ==="email") setEmail(value) 
+        else if(name==="password")setPassword(value)
     };
+
     const onSubmit =async(event) =>{
+        // 원치않을 때 실행되는 걸 처리
         event.preventDefault();
-        try{
-            let data;
-            if(newAccount){
-                // create Account
-                data = await createUserWithEmailAndPassword(authService , email, password);
-            } else{
-                // Login
-                data = await signInWithEmailAndPassword(authService , email,password);
-            }
-            console.log(data);
-        }catch(error){
-            setError(error.code);
-        }
+        // 계정생성인지 로그인인지 처리하여 data에 user정보를 담아줌.
+        try{ 
+            let data = newAccount ? await createUserWithEmailAndPassword(authService , email, password) // create Account
+            : await signInWithEmailAndPassword(authService , email,password); // Login
+        // 에러 정보를 저장함.
+        } catch(error) {setError(error.code);}
     }
+
     const toggleAccount = () => setNewAccount((prev)=>!prev);
+
+    // Return doc
     return (
         <div>
         <form onSubmit={onSubmit}>
@@ -45,7 +41,7 @@ const Auth = () => {
                 : error==="auth/email-already-in-use" ? "이미 사용중인 이메일입니다" 
                 : error==="auth/weak-password" ? "비밀번호를 강화해주세요(최소 6자 이상)" 
                 : error==="auth/operation-not-allowed" ? "전자 메일/암호 계정이 활성화되지 않았습니다"
-                : ""            
+                : ""
                 }
             </p>
         </form>
