5260bd9ac4e2653fcb611b4c623cfef524e8c836
sujk912 <aa991122aa@gmail.com>
2022-02-17 01:45:47 +0900

    setNweets

--- src/fBase.js
+++ src/fBase.js
@@ -2,18 +2,21 @@
 // import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
 import { initializeApp }  from 'firebase/app';
 import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
+import {getFirestore, addDoc, getDocs, setDoc, collection, query} from  'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: process.env.REACT_APP_API_KEY,
-authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
-projectId: process.env.REACT_APP_PROJECT_ID ,
-storageBucket:process.env.REACT_APP_STORAGE_BUCKET ,
-messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID ,
-appId: process.env.REACT_APP_APP_ID ,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+    projectId: process.env.REACT_APP_PROJECT_ID ,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET ,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID ,
+    appId: process.env.REACT_APP_APP_ID ,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const authService = getAuth();
 export {authService, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword,createUserWithEmailAndPassword};
+export const dbService = getFirestore();
+export {collection, addDoc, getDocs, setDoc, query};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,58 @@
-import React from "react";
+import { dbService, addDoc, getDocs, setDoc, collection,query} from "fBase";
+import React, { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    useEffect(()=>{
+        getNweets();
+    }, []);
+    const getNweets = async (event) =>{
+        const dbNweets = await getDocs(query(collection(dbService,"nweets")));
+        dbNweets.forEach((doc) => {
+            const nweetObject = {
+                ...doc.data(),
+                id:doc.id,
+            }
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    const onSubmit = async function (event) {
+        event.preventDefault();
+        ////////////////////////////////////////////// 여기 고치던중. 3.1의 4분 53초
+        try{
+            const docRef = await addDoc(
+                collection(dbService, "nweets"),
+                {
+                    nweet,
+                    createdAt : Date.now(),
+                }
+            );
+            console.log("Doc ID : ", docRef.id);
+        } catch(error){
+            console.log("Error adding Document : ", error);
+        }
+        setNweet("");
+    };
+    const onChange=(event)=>{
+        const {
+            target : {value},
+        } = event;
+        setNweet(value); 
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+            <div>
+                {nweets.map(nweet => 
+                    <div id={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>)}
+            </div>
+        </div>
+    );
+};
 export default Home;
