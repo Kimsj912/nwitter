f708e9bc832c66d6d47632134ba8f8836111b950
sujk912 <aa991122aa@gmail.com>
2022-02-17 04:39:15 +0900

    3장까지의 코드 정리

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,19 @@ import AppRouter from "components/Router";
 import {authService} from "../fBase"
 
 function App() {
-  // 로그인 여부를 판단
+  // States
   const [init, setInit] = useState(false);
-  const [isLoggedIn,setIsLoggedIn] = useState(false);
-  const [userObj, setUserObj] = useState("");
-  useEffect(()=>{ // React에게 컴포넌트가 렌더링 이후에 어떤 일을 수행해야하는 지
-    authService.onAuthStateChanged((user)=>{
-      if(user) {
-        setIsLoggedIn(true);
-        setUserObj(user);
-      }else setIsLoggedIn(false);
+  const [userObj, setUserObj] = useState(null); //다른페이지(home이외)에서 userObj를 사용하고싶어할 수 있기 때문이다.
+
+  useEffect(()=>{ // useEffect -> React에게 컴포넌트가 렌더링 이후에 어떤 일을 수행해야하는 지
+    authService.onAuthStateChanged((user)=>{ // 로그인, 로그아웃, 어플리케이션 초기화시 실행됨.
+      if(user)  setUserObj(user);
       setInit(true);
     });
   },[])
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing..."}
       <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
